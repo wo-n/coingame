9e2fb3bc10f099b6d45669fe85c689c52f3425a8
woon <woon@woon.local>
2016-04-12 22:32:10 +0900

    add file

---
+++ Assets/spawner.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+
+var prefab : Transform;
+function Update () {
+        if (Input.GetButtonDown ("Fire1")) {
+            var offs = Vector3(Mathf.Sin(Time.time * 7), 0, 0);
+            Instantiate (prefab, transform.position + offs , transform.rotation);
+        }
+}
